fdf0bb5df13fe488c9aff9ae7b293312d7b5dda6
arora-beep <sarthak.24bcs10150@sst.scaler.com>
2025-04-20 16:51:45 +0530

    changed card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Card = ({ meal }) => {
     return (
       <div className="card">
-        Hello
         <img src={meal.strMealThumb} alt={meal.strMeal} />
         <div className="content">
           <p>{meal.strMeal}</p>
