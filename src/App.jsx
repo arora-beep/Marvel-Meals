19071df26b6156d4b1d93039a9ed15a6f1c279e8
arora-beep <sarthak.24bcs10150@sst.scaler.com>
2025-04-20 17:45:09 +0530

    Syncing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from './context/GlobalState';
+import Header from './components/Header';
+import Home from "./pages/Home";
+import Meals from "./pages/Meals";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./style.css";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <AppProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/meals" element={<Meals />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  );
+};
 
-export default App
+export default App;
